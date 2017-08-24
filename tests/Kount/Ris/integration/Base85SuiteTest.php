d223d486be37a39e2135ddc23ec057660169e4e4
preslav-anev <preslav-anev@github.com>
2017-08-24 11:12:51 +0300

    KS-189 Refactored test suite.

--- tests/Kount/Ris/integration/Base85SuiteTest.php
+++ tests/Kount/Ris/integration/Base85SuiteTest.php
@@ -2,21 +2,19 @@
 
 class Base85SuiteTest extends PHPUnit_Framework_TestCase
 {
+
+  const PLAIN_TEXT = "This is sample text for testing purposes.";
+  const ENCODED_TEXT = "<+oue+DGm>F(&p)Ch4`2AU&;>AoD]4FCfN8Bl7Q+E-62?Df]K2/c";
+
   public function testEncode() {
-    $input = "This is sample text for testing purposes.";
-    $base85 = "<+oue+DGm>F(&p)Ch4`2AU&;>AoD]4FCfN8Bl7Q+E-62?Df]K2/c";
 
-    $encoded = base85::encode($input);
-
-    $this->assertEquals($encoded, $base85);
+    $encoded = base85::encode(self::PLAIN_TEXT);
+    $this->assertEquals($encoded, self::ENCODED_TEXT);
   }
 
   public function testDecode() {
-    $encoded = "<+oue+DGm>F(&p)Ch4`2AU&;>AoD]4FCfN8Bl7Q+E-62?Df]K2/c";
-    $expected = "This is sample text for testing purposes.";
 
-    $decoded = base85::decode($encoded);
-
-    $this->assertEquals($decoded, $expected);
+    $decoded = base85::decode(self::ENCODED_TEXT);
+    $this->assertEquals($decoded, self::PLAIN_TEXT);
   }
 }
