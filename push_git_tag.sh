aafc18dd8343d3248ff772e28611e2e335054bc7
sanjeev.kumar <sanjeev.kumar@kount.com>
2020-07-09 22:14:55 +0530

    Changes for PHP RIS SDKs testing with packagist

--- push_git_tag.sh
+++ push_git_tag.sh
@@ -5,8 +5,8 @@ set -e
 set -u
 
 # decrypt the private key
-echo "$SSH_PRIVATE_KEY" > ./gitlab-deploy-key
-chmod 400 ./gitlab-deploy-key
+# echo "$SSH_PRIVATE_KEY" > ./gitlab-deploy-key
+# chmod 400 ./gitlab-deploy-key
 
 # set the git username/email to be able to perform git operations
 echo git version: $(git --version)
@@ -30,6 +30,6 @@ git tag -a $VERSION -m "Setting version as tag during build."
 # push the git tag
 # leverage GIT_SSH option to use a dedicated SSH key
 # see https://git-scm.com/docs/git#git-codeGITSSHcode for documentation of this feature
-echo 'ssh -i ./gitlab-deploy-key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $*' > ssh
+echo 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $*' > ssh
 chmod +x ssh
 GIT_SSH='./ssh' git push origin $VERSION
