52109b812afab0cf3a9f6bcb516d21a2278bd7b0
CORP\Vishalakshi.V <vishalakshi.v@kount.com>
2022-03-11 17:10:10 +0530

    Modified UtilityHelperTest file to get CONFIG_KEY from environment

--- tests/Kount/Ris/integration/KhashFunctionalityTest.php
+++ tests/Kount/Ris/integration/KhashFunctionalityTest.php
@@ -9,6 +9,12 @@ class ConfigurationSaltTest extends \PHPUnit\Framework\TestCase
   const  MERCHANT_ID = 666666;
 
   public function testCorrectCreditCard1() {
+    
+    $configReader = Kount_Util_ConfigFileReader::instance();
+    if ($configReader->getConfigSetting('CONFIG_KEY') == '$CONFIG_KEY') {
+      Kount_Util_Khash::setConfigKey(base64_decode(getenv("RIS_CONFIG_KEY_BASE64")));
+    }
+    
     $token = Kount_Util_Khash::hashPaymentToken(self::CARD_NUM_1);
 
     $this->assertEquals('411111WMS5YA6FUZA1KC', $token);
@@ -42,5 +48,11 @@ class ConfigurationSaltTest extends \PHPUnit\Framework\TestCase
     $token = Kount_Util_Khash::hashGiftCard(self::MERCHANT_ID, self::CARD_NUM_3);
 
     $this->assertEquals('666666FEXQI1QS6TH2O5', $token);
+  }
+
+  public function testHashWithNull() {
+    $token = Kount_Util_Khash::hashPaymentToken(null);
+
+    $this->assertEquals('TBNC2MTR28JOUY', $token);
   }
 }
