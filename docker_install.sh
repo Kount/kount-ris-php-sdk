792560506e974b2ea2a10b7bc180f6db3a298dd5
Greg Tyler <daniel.tyler@equifax.com>
2025-08-25 07:25:09 -0600

    update ca certs

--- docker_install.sh
+++ docker_install.sh
@@ -5,14 +5,9 @@
 
 set -xe
 
-# Install git (the php image doesn't have it) which is required by composer
-apt-get update -yqq
-apt-get install git -yqq
-
-# Install phpunit, the tool that we will use for testing
-curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
-chmod +x /usr/local/bin/phpunit
-
-# Install mysql driver
-# Here you can install any other extension that you need
-docker-php-ext-install pdo_mysql
+apk add ca-certificates zip unzip git curl libzip-dev libpng-dev libxml2-dev oniguruma-dev
+docker-php-ext-install pdo pdo_mysql zip gd bcmath xml
+update-ca-certificates
+curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar && chmod +x /usr/local/bin/phpunit
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+export PATH="/usr/local/bin:$PATH"
